de868737bdbeaab2133cb3999e7405ab5c3e363d
DomTorres <neildominictorres@gmail.com>
2024-06-22 23:41:50 -0700

    added backend route for messages

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("./utils.js");
 require('dotenv').config();
 
 const express = require('express');
+const fs = require("fs");
 const app = express();
 const MongoStore = require('connect-mongo');
 const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
@@ -19,12 +21,26 @@ var mongoStore = MongoStore.create({
     }
 })
 
+app.get("/messages", (req, res) => {
+    // bugsnag.start();
+    let messages = fs.readFileSync("./messages.html", "utf8");
+    res.send(messages);
+})
+
+app.get("/home", (req, res) => {
+    // TO DO: Retrieve matched users using algorithm
+    const matchedUsers = [{
+        name: "John",
+        id: "123",
+    }];
+
+    res.render("home", { matchedUsers });
+})
 
 app.get("*", (req, res) => {
     res.status(404);
     res.send("Page not found - 404");
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
